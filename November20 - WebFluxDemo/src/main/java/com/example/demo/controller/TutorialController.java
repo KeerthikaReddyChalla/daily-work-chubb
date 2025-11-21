3dd0539f20d06e2cb9f020f2cfcf0fbe515fb6aa
KeerthikaReddyChalla <keerthikareddy2005@gmail.com>
2025-11-21 10:12:26 +0530

    updated controller

--- November20 - WebFluxDemo/src/main/java/com/example/demo/controller/TutorialController.java
+++ November20 - WebFluxDemo/src/main/java/com/example/demo/controller/TutorialController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.Tutorial;
@@ -16,32 +18,50 @@ public class TutorialController {
     @Autowired
     TutorialService tutorialService;
 
+
     @GetMapping("/tutorials")
+    @ResponseStatus(HttpStatus.OK)
     public Flux<Tutorial> getAll() {
         return tutorialService.findAll();
     }
 
     @GetMapping("/tutorials/{id}")
-    public Mono<Tutorial> getById(@PathVariable int id) {
-        return tutorialService.findById(id);
+    public Mono<ResponseEntity<Tutorial>> getById(@PathVariable int id) {
+        return tutorialService.findById(id)
+            .map(tutorial -> ResponseEntity.ok(tutorial)) // 200
+            .defaultIfEmpty(ResponseEntity.notFound().build()); // 404
     }
 
+
     @PostMapping("/tutorials")
+    @ResponseStatus(HttpStatus.CREATED)
     public Mono<Integer> create(@RequestBody Tutorial t) {
         return tutorialService
             .save(new Tutorial(t.getTitle(), t.getDescription(), false))
-            .map(Tutorial::getId);  
+            .map(Tutorial::getId); // return only id
     }
 
 
     @PutMapping("/tutorials/{id}")
-    public Mono<Tutorial> update(@PathVariable int id, @RequestBody Tutorial t) {
-        return tutorialService.update(id, t);
+    public Mono<ResponseEntity<Tutorial>> update(
+            @PathVariable int id,
+            @RequestBody Tutorial t) {
+
+        return tutorialService.update(id, t)
+            .map(updated -> ResponseEntity.ok(updated)) // 200
+            .defaultIfEmpty(ResponseEntity.notFound().build()); // 404
     }
 
     @DeleteMapping("/tutorials/{id}")
-    public Mono<Void> delete(@PathVariable int id) {
-        return tutorialService.deleteById(id);
-    }
-}
+    public Mono<ResponseEntity<Void>> delete(@PathVariable int id) {
 
+        return tutorialService.findById(id)
+            .flatMap(existing ->
+                tutorialService.deleteById(id)
+                    .then(Mono.<ResponseEntity<Void>>just(ResponseEntity.noContent().build())) // 204
+            )
+            .defaultIfEmpty(ResponseEntity.notFound().build()); // 404
+    }
+
+
+}
