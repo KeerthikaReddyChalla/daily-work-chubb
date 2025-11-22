d4fa3ed5ae620b12c27f2e9096047a3cfd9dac82
KeerthikaReddyChalla <keerthikareddy2005@gmail.com>
2025-11-22 17:36:28 +0530

    added MongoDB WebFlux

---
+++ November22 - ReactiveMongoDB/src/main/java/com/example/reactive/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.example.reactive.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import com.example.reactive.model.Tutorial;
+import reactor.core.publisher.Flux;
+
+public interface TutorialRepository extends ReactiveMongoRepository<Tutorial, String> {
+
+    Flux<Tutorial> findByPublished(boolean published);
+
+    Flux<Tutorial> findByTitleContaining(String title);
+}
